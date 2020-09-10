fa10705431b028cfb73bcb764e166ec924b43f96
Saloni Gundawar <58361655+SaloniGundawar@users.noreply.github.com>
2020-09-10 19:09:22 +0530

    created assigning_list_ele.py
    assigning elements to different lists

---
+++ assigning_list_ele.py
@@ -0,0 +1,13 @@
+list1=[1,2,3,4,5]
+list2=[20,25,66,99,33,56]
+list3=["i","am","a","good","girl"]
+list4=["mangoes", "apples", "berries"]
+
+list4.append("bananas")
+print(list4)
+
+list1.insert(3,10)
+print(list1)
+
+list4=list2+list3
+print(list4)
